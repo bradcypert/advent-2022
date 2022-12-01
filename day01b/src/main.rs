1fb5a4a957d6d652dbdca699ae9151ce9705ecff
Brad Cypert <brad.cypert@gmail.com>
2022-12-01 14:27:31 -0500

    day01b

---
+++ day01b/src/main.rs
@@ -0,0 +1,23 @@
+use std::io::{self, Write, ErrorKind, Error};
+use std::fs;
+
+fn main() -> io::Result<()> {
+    let args = std::env::args().collect::<Vec<String>>();
+    println!("{:?}", args);
+    if let Some(file) = args.get(1) {
+        let input = fs::read_to_string(&file).unwrap();
+        let inventories = input.trim().split("\n\n").map(|x| {
+            x.lines()
+                .filter_map(|x| x.parse::<i32>().ok())
+                .sum::<i32>()
+        });
+
+        let mut inven_vec = inventories.collect::<Vec<i32>>();
+        inven_vec.sort();
+        inven_vec.reverse();
+        io::stdout().write_all(format!("{}", &inven_vec[..3].iter().sum::<i32>()).as_bytes())?;
+        Ok(())
+    } else {
+        Err(Error::new(ErrorKind::InvalidInput, "You must provide a filename for the input."))
+    }
+}
